290c241041d33c32e7bf8d9d3346cb29ec10b41b
Ana <aninha10.desouza@gmail.com>
2023-07-02 17:39:14 -0300

    Alteração na nomenclatura dos arquivos

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,36 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import CabecalhoComp from "@/components/CabecalhoComp.vue";
+// import RodapeComp from "@/components/RodapeComp.vue";
+// import HomeViewVue from "./views/HomeView.vue";
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>
+    <CabecalhoComp />
+    <main>
+      <RouterView />
+    </main>
+    <!-- <RodapeComp /> -->
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style>
+@import "@/assets/base.css";
+@import "@/assets/main.css";
+#app {
+  max-width: 5000px;
+  max-height: 1000px;
+  margin: 0 auto;
+  height: 100vh;
+  font-weight: normal;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+header,
+footer {
+  background-color: rgb(126, 73, 126);
+  color: white;
+  height: 15%;
+  font-size: 1.3rem;
+  display: flex;
+  align-items: center;
+  padding-left: 2rem;
 }
 </style>
---
+++ src/components/CabecalhoComp.vue
@@ -0,0 +1,14 @@
+<script setup>
+import AppBarComp from "@/components/AppBarComp.vue";
+</script>
+<template>
+  <div class="menu-direito">
+    <AppBarComp />
+  </div>
+</template>
+
+<style>
+/* .menu-direito {
+  width: 100%;
+} */
+</style>
